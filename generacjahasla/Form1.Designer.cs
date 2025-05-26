49fde24ed3de43f00777844546b55d7bd0201bd1
xHelpy <xiozeerpl@gmail.com>
2025-05-26 11:49:27 +0200

    2

--- generacjahasla/Form1.Designer.cs
+++ generacjahasla/Form1.Designer.cs
@@ -30,13 +30,22 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.userSurname = new System.Windows.Forms.Label();
             this.userName = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.generatePassword = new System.Windows.Forms.Button();
+            this.characterAmount = new System.Windows.Forms.TextBox();
+            this.specialCharacters = new System.Windows.Forms.CheckBox();
+            this.numbers = new System.Windows.Forms.CheckBox();
+            this.letters = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -57,10 +66,24 @@
             this.groupBox1.Controls.Add(this.userName);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(281, 317);
+            this.groupBox1.Size = new System.Drawing.Size(281, 183);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Dane Pracownika";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tester",
+            "Starszy Programista",
+            "Młodszy Programista",
+            "Kierownik"});
+            this.comboBox1.Location = new System.Drawing.Point(143, 125);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label2
             // 
@@ -68,9 +91,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.Location = new System.Drawing.Point(23, 123);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.Size = new System.Drawing.Size(91, 20);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Nazwisko";
+            this.label2.Text = "Stanowisko";
             // 
             // textBox2
             // 
@@ -78,6 +101,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox1
             // 
@@ -85,6 +109,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // userSurname
             // 
@@ -105,32 +130,110 @@
             this.userName.Size = new System.Drawing.Size(39, 20);
             this.userName.TabIndex = 2;
             this.userName.Text = "Imię";
+            this.userName.Click += new System.EventHandler(this.userName_Click);
             // 
-            // comboBox1
+            // button1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Tester",
-            "Starszy Programista",
-            "Młodszy Programista",
-            "Kierownik"});
-            this.comboBox1.Location = new System.Drawing.Point(143, 125);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 7;
+            this.button1.Location = new System.Drawing.Point(352, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Zatwierdź";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.generatePassword);
+            this.groupBox2.Controls.Add(this.characterAmount);
+            this.groupBox2.Controls.Add(this.specialCharacters);
+            this.groupBox2.Controls.Add(this.numbers);
+            this.groupBox2.Controls.Add(this.letters);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(380, 15);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(306, 180);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Generacja hasła";
+            // 
+            // generatePassword
+            // 
+            this.generatePassword.Location = new System.Drawing.Point(99, 151);
+            this.generatePassword.Name = "generatePassword";
+            this.generatePassword.Size = new System.Drawing.Size(103, 23);
+            this.generatePassword.TabIndex = 11;
+            this.generatePassword.Text = "Generuj Hasło";
+            this.generatePassword.UseVisualStyleBackColor = true;
+            this.generatePassword.Click += new System.EventHandler(this.generatePassword_Click);
+            // 
+            // characterAmount
+            // 
+            this.characterAmount.Location = new System.Drawing.Point(119, 50);
+            this.characterAmount.Name = "characterAmount";
+            this.characterAmount.Size = new System.Drawing.Size(100, 20);
+            this.characterAmount.TabIndex = 10;
+            this.characterAmount.TextChanged += new System.EventHandler(this.characterAmount_TextChanged);
+            // 
+            // specialCharacters
+            // 
+            this.specialCharacters.AutoSize = true;
+            this.specialCharacters.Location = new System.Drawing.Point(24, 127);
+            this.specialCharacters.Name = "specialCharacters";
+            this.specialCharacters.Size = new System.Drawing.Size(103, 17);
+            this.specialCharacters.TabIndex = 9;
+            this.specialCharacters.Text = "Znaki Specjalne";
+            this.specialCharacters.UseVisualStyleBackColor = true;
+            this.specialCharacters.CheckedChanged += new System.EventHandler(this.specialCharacters_CheckedChanged);
+            // 
+            // numbers
+            // 
+            this.numbers.AutoSize = true;
+            this.numbers.Location = new System.Drawing.Point(24, 104);
+            this.numbers.Name = "numbers";
+            this.numbers.Size = new System.Drawing.Size(49, 17);
+            this.numbers.TabIndex = 8;
+            this.numbers.Text = "Cyfry";
+            this.numbers.UseVisualStyleBackColor = true;
+            this.numbers.CheckedChanged += new System.EventHandler(this.numbers_CheckedChanged);
+            // 
+            // letters
+            // 
+            this.letters.AutoSize = true;
+            this.letters.Location = new System.Drawing.Point(24, 80);
+            this.letters.Name = "letters";
+            this.letters.Size = new System.Drawing.Size(115, 17);
+            this.letters.TabIndex = 7;
+            this.letters.Text = "Małe i wielkie litery";
+            this.letters.UseVisualStyleBackColor = true;
+            this.letters.CheckedChanged += new System.EventHandler(this.letters_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(20, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ile znaków?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 339);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,6 +249,14 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label userSurname;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox specialCharacters;
+        private System.Windows.Forms.CheckBox numbers;
+        private System.Windows.Forms.CheckBox letters;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox characterAmount;
+        private System.Windows.Forms.Button generatePassword;
     }
 }
 
--- generacjahasla/Form1.cs
+++ generacjahasla/Form1.cs
@@ -12,12 +12,139 @@ namespace generacjahasla
 {
     public partial class Form1 : Form
     {
+        public int passwordLength = 0;
+        public bool characterState = false;
+        public bool numbersState = false;
+        public bool lettersState = false;
+
+
+        public string position = "";
+        public string name = "";
+        public string surname = "";
+
+        public string Password;
+        public string smalLetters = "abcdefghijklmnopqrstuvwxyz";
+        public string bigLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public string specialCharset = "!@#$%^&*()_-+=";
+        public string numberCharset = "1234567890";
+
+        Random random = new Random();
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void specialCharacters_CheckedChanged(object sender, EventArgs e)
+        {
+            if(!characterState)
+                characterState = true;
+            else
+                characterState = false;
+        }
+
+        private void numbers_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!numbersState)
+                numbersState = true;
+            else
+                numbersState = false;
+        }
+
+        private void letters_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!lettersState)
+                lettersState = true;
+            else
+                lettersState = false;
+        }
+
+        private void characterAmount_TextChanged(object sender, EventArgs e)
+        {
+            passwordLength = int.Parse( characterAmount.Text );
+        }
+
+        private void generatePassword_Click(object sender, EventArgs e)
+        {
+            Password = "";
+            for (int i = 0; i < passwordLength; i++)
+            {
+                if (i == 0 && characterState)
+                {
+                    int x = random.Next(specialCharset.Length);
+
+                    Password += specialCharset[x];
+
+                } else if(i == 0 && !characterState)
+                {
+                    Console.WriteLine("1");
+                    int x = random.Next(smalLetters.Length);
+
+                    Password += smalLetters[x];
+                }
+                if (i == 1 && numbersState)
+                {
+                    int x = random.Next(numberCharset.Length);
+
+                    Password += numberCharset[x];
+                }
+                else if (i == 1 && !numbersState)
+                {
+                    int x = random.Next(smalLetters.Length);
+
+                    Password += smalLetters[x];
+                }
+                if (i == 2 && lettersState) 
+                    {
+                    int x = random.Next(bigLetters.Length);
+
+                    Password += bigLetters[x];
+                }
+                else if (i == 2 && !lettersState)
+                {
+                    int x = random.Next(smalLetters.Length);
+
+                    Password += smalLetters[x];
+                }
+                if (i > 2)
+                {
+                    int x = random.Next(smalLetters.Length);
+
+                    Password += smalLetters[x];
+                }
+                
+            }
+      
+            MessageBox.Show(Password);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            surname = textBox2.Text;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            position = comboBox1.Text.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(name + " " + surname + " " + position + " " + Password);
+        }
+
+        private void userName_Click(object sender, EventArgs e)
         {
 
         }
